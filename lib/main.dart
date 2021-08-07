8c0b915054b5e1ad195c182e9e83d2e708dfb9e0
Shanoop <muhammadshanoopek@cet.ac.in>
2021-08-07 17:26:15 +0530

    Updation

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Welcome Shanoop",
+      title: "Welcome Shanoop E K",
       theme: ThemeData(
           primaryColor: Colors.black,
           shadowColor: Colors.amber,
